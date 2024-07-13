0bfb634edb5c4910c33563b81168fca1bec4ee19
Nick Jackson <nick@dxw.com>
2024-07-13 12:01:58 +0100

    Improved data, price comparison etc

--- README.md
+++ README.md
@@ -4,6 +4,23 @@ This repository contains all the code to produce the price list for the bar at S
 
 ## Updating price data
 
+### Getting the source price report
+
+1. Sign in to ICRTouch
+1. Go to 'Reports/View'
+1. Generate a 'PLU Price List' CSV report for All Groups, All Departments and All Price Levels
+1. Put it in the `data` folder
+
+### Running a sense check
+
+To sense check the till's PLU report against the price list data, run
+
+```
+poetry run python app.py check FILENAME
+```
+
+### Updating the consolidated price list
+
 All the price data lives in the `data/prices.yaml` file.
 
 ## Generating price lists
--- app.py
+++ app.py
@@ -1,29 +1,138 @@
-from weasyprint import HTML, CSS
-import os
-from weasyprint.text.fonts import FontConfiguration
-import click
-from jinja2 import Environment, PackageLoader, select_autoescape
+import csv
 from datetime import datetime
-import yaml
+from pathlib import Path
+from typing import TypedDict
 
-OUTPUT_DIR = os.path.join(os.path.dirname(__file__), "outputs")
+import click
+import pytz
+import yaml
+from jinja2 import Environment, PackageLoader, select_autoescape
+from weasyprint import CSS, HTML
+from weasyprint.text.fonts import FontConfiguration
+
+DATA_DIR = Path(__file__).parent / "data"
+OUTPUT_DIR = Path(__file__).parent / "outputs"
+
+
+class PluPrice(TypedDict):
+    price: int
 
 
 def format_currency(value) -> str:
-    return "£{:,.2f}".format(value / 100)
+    return f"£{value / 100:,.2f}"
 
 
 def is_list(value) -> bool:
     return isinstance(value, list)
 
 
-@click.command()
+def plu_report_row_discardable(row) -> bool:
+    # Skip if PLU is empty
+    if row["plu"] == "":
+        return True
+
+    # Skip if PLU is not numeric
+    if not row["plu"].isnumeric():
+        return True
+
+    # Skip if name begins with 'PLU'
+    if row["name"].startswith("PLU"):
+        return True
+
+    # Skip if name begins with 'XXX'
+    if row["name"].startswith("XXX"):
+        return True
+
+    return False
+
+
+def write_html_to_pdf_with_styles(html: str, output_filename: str) -> None:
+    font_config = FontConfiguration()
+
+    with (Path(__file__).parent / "styles" / "styles.css").open() as css_file:
+        css = CSS(string=css_file.read(), font_config=font_config)
+
+    pdf = HTML(string=html)
+
+    pdf.write_pdf(
+        OUTPUT_DIR / output_filename,
+        stylesheets=[css],
+        font_config=font_config,
+    )
+
+
+def get_all_plus_in_price_list() -> dict[int, PluPrice]:
+    with (DATA_DIR / "prices.yaml").open() as prices_file:
+        price_data = yaml.safe_load(prices_file)
+
+    plus: dict[int, PluPrice] = {}
+
+    for category in price_data:
+        for item in category["items"]:
+            if isinstance(item, list):
+                for variant in item:
+                    if "plus" in variant:
+                        for plu in variant["plus"]:
+                            plus[plu] = {"price": variant["price"]}
+            elif "plus" in item:
+                for plu in item["plus"]:
+                    plus[plu] = {"price": item["price"]}
+
+    return plus
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument("file", type=click.File("r"))
+def check(file):
+    click.echo("Sense checking price list…")
+
+    products: list[dict] = []
+
+    for row in csv.DictReader(file, fieldnames=("plu", "code", "name", "price")):
+        if plu_report_row_discardable(row):
+            continue
+
+        products.append(row)
+
+    plus_in_price_list = get_all_plus_in_price_list()
+
+    for product in products:
+        product_price = round(float(product["price"]) * 100)
+
+        if int(product["plu"]) not in plus_in_price_list:
+            click.echo(
+                click.style(
+                    f'MISSING: PLU {product["plu"]} ({product["name"]}) is missing from price list, till price is {product_price}',
+                    fg="red",
+                ),
+            )
+            continue
+
+        product_plu_price = plus_in_price_list[int(product["plu"])]
+
+        if product_plu_price["price"] != product_price:
+            click.echo(
+                click.style(
+                    f'PRICE MISMATCH: PLU {product["plu"]} ({product["name"]}) is priced {product_price} on the till but {product_plu_price["price"]} on the list',
+                    fg="red",
+                ),
+            )
+
+    click.echo(click.style("Done!", fg="green"))
+
+
+@cli.command()
 def build():
-    with open(
-        os.path.join(os.path.dirname(__file__), "data", "prices.yaml")
-    ) as prices_file:
+    click.echo("Building outputs…")
+
+    with (DATA_DIR / "prices.yaml").open() as prices_file:
         data = {
-            "generated_time": datetime.now().strftime("%Y-%m-%d %H:%M"),
+            "generated_time": datetime.now(tz=pytz.timezone("Europe/London")).strftime("%Y-%m-%d %H:%M"),
             "prices_data": yaml.safe_load(prices_file),
         }
 
@@ -32,40 +141,18 @@ def build():
     env.filters["format_currency"] = format_currency
     env.filters["is_list"] = is_list
 
-    font_config = FontConfiguration()
-
     a3_template = env.get_template("A3.jinja")
     a3_html = a3_template.render(**data)
 
-    pdf = HTML(string=a3_html)
-
-    with open(
-        os.path.join(os.path.dirname(__file__), "styles", "styles.css"), "r"
-    ) as css_file:
-        css = CSS(string=css_file.read(), font_config=font_config)
-
-    pdf.write_pdf(
-        os.path.join(OUTPUT_DIR, "A3.pdf"),
-        stylesheets=[css],
-        font_config=font_config,
-    )
+    write_html_to_pdf_with_styles(a3_html, "A3.pdf")
 
     a5_template = env.get_template("A5.jinja")
     a5_html = a5_template.render(**data)
 
-    pdf = HTML(string=a5_html)
+    write_html_to_pdf_with_styles(a5_html, "A5.pdf")
 
-    with open(
-        os.path.join(os.path.dirname(__file__), "styles", "styles.css"), "r"
-    ) as css_file:
-        css = CSS(string=css_file.read(), font_config=font_config)
-
-    pdf.write_pdf(
-        os.path.join(OUTPUT_DIR, "A5.pdf"),
-        stylesheets=[css],
-        font_config=font_config,
-    )
+    click.echo(click.style("Done!", fg="green"))
 
 
 if __name__ == "__main__":
-    build()
+    cli()
